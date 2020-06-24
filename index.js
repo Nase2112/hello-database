c8c10bc48b9998f8ff931f27a4d5e9e8c10271a6
Angel <angelyucra03@hotmail.com>
2020-06-23 22:16:05 -0300

    modificaciones 2

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // ahora tambien importamos mongoose
 const express = require('express');
 const mongoose = require('mongoose');
+const cors     = require('cors');
+
 // importamos el router que creamos para la api
 const router = require('./routes/api/user');
+
 // puerto y base de datos
 const port = process.env.PORT || 3000;
 const db = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
+
 const app = express();
+
+app.use = express();
+
 // conexion a la base de datos
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
@@ -16,9 +23,12 @@ mongoose
 console.log(`DB connected @ ${db}`);
 })
 .catch(err => console.error(`Connection error ${err}`));
+
 // usamos el router
+
 app.use('/api', router);
 // el server escucha todo
+
 app.listen(port, () => {
 
 console.log(`Server listening on port ${port}`);
